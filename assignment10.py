c3edc25f321fd479c5b8492c302fab8aab886b36
Ironrascal <ironrascal@gmail.com>
2014-12-11 00:13:14 -0500

    Update assignment10.py

--- assignment10.py
+++ assignment10.py
@@ -10,13 +10,14 @@ for line in handle:
         time = line[5].split(":")
         hour = time[0]
         counts[hour] = counts.get(hour, 0) + 1
-                
+        
+
 ord = list()
 for k, v in counts.items():
-    newlist = (k, v)
-    ord.append(newlist)
-    ord.sort(newlist)
-    print ord
-    for k, v in ord.list():
-        print k, v
+    counts[hour] = counts.get(hour, 0) + 1
+    results = ((k, v))
+    ord.append(results)
+ord.sort()
     
+for k,v in ord:
+    print k[:2], v
